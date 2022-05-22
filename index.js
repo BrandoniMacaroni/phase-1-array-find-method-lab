976ba2a4fead96a6222271d84ca9e78adbbec184
Brandon <bcharris713@gmail.com>
2022-05-22 14:26:07 -0500

    commit tree 4ec54decca21c4dec6a67228c36490efe3e8aa6e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let obj = record.find(o => o.result === 'W');
+    if (obj != undefined) {
+        let yearWon = obj.year;
+        return yearWon;
+    }
+    return obj;
+}
